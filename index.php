3094848e4d3f5fd2c7f4d41e0e18a6ee178882ea
Tritoq Agência Digital <tritoq@gmail.com>
2012-10-17 16:57:45 -0300

    P1

--- index.php
+++ index.php
@@ -1,8 +1,31 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Trabalho 01 de Programação 2
+ * 
+ * @author Artur T. Magalhães Jr <artur@tritoq.com>
+ * @version 1
  */
-echo "Hello World";
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Trabalho T0 - Programação 2</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="UTF-8">
+        <!-- CSS Document Link -->
+        <link media="screen" rel="stylesheet" type="text/css" href="css/css.css" />        
+        <link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+    </head>
+    <body>
+        <!-- Header of the document -->
+        <header class="content">
+            
+        </header>        
+        <div class="content" id="content">
+            
+        </div>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
